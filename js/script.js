79747acfb92ed7d610005756f43de6e6654f1348
Erin <erin.nienhuis@gmail.com>
2019-07-22 21:15:10 -0400

    Pushed pokemon into array and looped through the array to write the pokemon name into the DOM

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ var bulbasaur = {
   number = 1
 };
 
+repository.push(bulbasaur);
+
 var squirtle = {
   name = 'Squirtle',
   height = .5,
@@ -14,9 +16,17 @@ var squirtle = {
   number = 7
 };
 
+repository.push(squirtle);
+
 var charmander = {
   name = 'Charmander',
   height = .6,
   types = ['fire'],
   number = 4
 };
+
+repository.push(charmander);
+
+for (var i = 0; i < repository.length; i++) {
+  document.write(repository[i].name);
+}
