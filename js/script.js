f4343652ae3cb8bf1b771514e8d6b9d2ed464fb2
Erin <erin.nienhuis@gmail.com>
2019-07-22 21:08:25 -0400

    Defined 3 Pokemon objects

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var repository = [];
+
+var bulbasaur = {
+  name = 'Bulbasaur',
+  height = .7,
+  types = ['grass','poison'],
+  number = 1
+};
+
+var squirtle = {
+  name = 'Squirtle',
+  height = .5,
+  types = ['water'],
+  number = 7
+};
+
+var charmander = {
+  name = 'Charmander',
+  height = .6,
+  types = ['fire'],
+  number = 4
+};
