78694292acba1e7565a946865c8020ae439dc285
Erin <erin.nienhuis@gmail.com>
2019-07-22 21:19:34 -0400

    Fixed the objects to use colons instead of equal signs.

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
 var repository = [];
 
 var bulbasaur = {
-  name = 'Bulbasaur',
-  height = .7,
-  types = ['grass','poison'],
-  number = 1
+  name: 'Bulbasaur',
+  height: .7,
+  types: ['grass','poison'],
+  number: 1
 };
 
 repository.push(bulbasaur);
 
 var squirtle = {
-  name = 'Squirtle',
-  height = .5,
-  types = ['water'],
-  number = 7
+  name: 'Squirtle',
+  height: .5,
+  types: ['water'],
+  number: 7
 };
 
 repository.push(squirtle);
 
 var charmander = {
-  name = 'Charmander',
-  height = .6,
-  types = ['fire'],
-  number = 4
+  name: 'Charmander',
+  height: .6,
+  types: ['fire'],
+  number: 4
 };
 
 repository.push(charmander);
 
 for (var i = 0; i < repository.length; i++) {
-  document.write(repository[i].name);
-}
+  document.write(repository[i].name)
+};
