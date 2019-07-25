f5b79fcb432d5809275e5e26d5511779558a03b2
Erin <erin.nienhuis@gmail.com>
2019-07-24 20:31:35 -0400

    Changed how the repository array was setup with pokemon objects

--- js/script.js
+++ js/script.js
@@ -1,31 +1,22 @@
-var repository = [];
-
-var bulbasaur = {
+var repository = [
+{
   name: 'Bulbasaur',
   height: .7,
   types: ['grass','poison'],
   number: 1
-};
-
-repository.push(bulbasaur);
-
-var squirtle = {
+},
+{
   name: 'Squirtle',
   height: .5,
   types: ['water'],
   number: 7
-};
-
-repository.push(squirtle);
-
-var charmander = {
+},
+{
   name: 'Charmander',
   height: .6,
   types: ['fire'],
   number: 4
-};
-
-repository.push(charmander);
+}]
 
 var heightTxt = ' (Height: ';
 var bigHighlight = ') - Wow, that\'s big! </p>';
